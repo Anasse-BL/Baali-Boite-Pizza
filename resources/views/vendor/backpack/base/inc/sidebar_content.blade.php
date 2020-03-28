1b6135174e70cacffb066662ee44ac3726fb449c
Anasse-BL <57466499+Anasse-BL@users.noreply.github.com>
2020-03-28 22:05:21 +0100

    Fixing Bugs

---
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -0,0 +1,6 @@
+<!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('tag') }}'><i class='nav-icon fa fa-question'></i> Tags</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('article') }}'><i class='nav-icon fa fa-question'></i> Articles</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('catproduit') }}'><i class='nav-icon fa fa-question'></i> Catproduits</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('produit') }}'><i class='nav-icon fa fa-question'></i> Produits</a></li>
